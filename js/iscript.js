353ef940a3db90c9581b931362cde072b13ebc00
KCPIT <kgu090716@gmail.com>
2020-03-14 23:23:38 +0900

    teat

---
+++ js/iscript.js
@@ -0,0 +1,4 @@
+function asearch(address){
+    address = document.getElementById("search").value;
+    window.location.href="address.html?address="+address;
+}
---
+++ js/script - 복사본.js
@@ -0,0 +1,37 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var adr = getParameterByName('address');
+
+alert (adr);
+
+$(document).ready(function(){
+    $('#userTable').dataTable({
+        pageLength: 3,
+        bPaginate: true,
+        bLengthChange: true,
+        lengthMenu : [ [ 3, 5, 10, -1 ], [ 3, 5, 10, "All" ] ],
+        bAutoWidth: false,
+        processing: true,
+        ordering: true,
+        serverSide: false,
+        searching: true,
+        ajax : {
+            "url":"https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json",
+            "type":"GET",
+            "data": { address:"경기도 시흥시"},
+            'dataSrc':'stores'
+        },
+        columns : [
+            {data: "addr"},
+            {data: "name"},
+            {data: "created_at"}
+        ]
+
+    });
+
+});
